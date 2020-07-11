f691283e5c75626c9d2ed9767d81710c84bd1216
Dino Stancic <dinostancic7@gmail.com>
2020-07-11 21:35:35 +0200

    Page was built but never used

---
+++ javascript/media.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    AOS.init();
+    $('body').css('display', 'none');
+    $('body').slideDown(1000); 
+    $('#navigation').hide();
+    $('.show').click(function(){
+        $(this).hide();
+        $('#navigation').slideDown(1000);
+        $('.social').hide();
+    });
+      $('.showup').click(function(){
+        $('#navigation').slideUp(1000);
+        $('.show').delay(1000).fadeIn();
+    });
+    $('.social').stick_in_parent();
+});
---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('body').css('display', 'none');
+    $('body').fadeIn(1000); 
+    $('#navigation').hide();
+    $('.show').click(function(){
+        $(this).hide();
+        $('#navigation').slideDown(1000);
+    });
+      $('.showup').click(function(){
+        $('#navigation').slideUp(1000);
+        $('.show').delay(1000).fadeIn();
+    });
+    $('.wrapper').backstretch([
+        'img/background/bg1.jpg',
+        'img/background/bg2.jpg',
+        'img/background/bg3.jpg',
+        'img/background/bg4.jpg',
+        'img/background/bg5.jpg'
+    ],{
+        fade: 3000,
+        duration: 2000
+    });
+
+});
